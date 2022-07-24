890ac4f18a5cd47461a954bfb6575a7a3b0a6ffb
Fireflyest <fireflyest@qq.com>
2022-07-24 10:49:50 +0800

    管理下架玩家物品 指令别名更改

---
+++ src/main/java/com/fireflyest/market/command/MarketAdminTab.java
@@ -0,0 +1,41 @@
+package com.fireflyest.market.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarketAdminTab implements TabCompleter {
+
+    private final List<String> op = new ArrayList<>();
+
+    public MarketAdminTab(){
+        op.add("cancel");
+        op.add("black");
+    }
+
+    public List<String> onTabComplete(@NotNull CommandSender sender, Command command, @NotNull String alias, String[] args){
+        if(command.getName().equalsIgnoreCase("market") || command.getName().equalsIgnoreCase("gm")){
+            List<String> tab = new ArrayList<>();
+            if(args.length == 1){
+                for(String sub : op){
+                    if(sub.startsWith(args[0]))tab.add(sub);
+                }
+            }else if(args.length == 2){
+                if("cancel".equalsIgnoreCase(args[0])){
+                    tab.add("<id>");
+                }else if ("black".equalsIgnoreCase(args[0])){
+                    return null;
+                }
+            }else if(args.length == 3){
+               return tab;
+            }
+            return tab;
+        }
+        return null;
+    }
+
+}
